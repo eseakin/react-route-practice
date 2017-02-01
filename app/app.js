1f43a2013b5586694bd70d9f9985b68eb7da9f2b
Eric Eakin <eseakin@gmail.com>
2017-02-01 02:10:00 -0800

    Added sub routes to main routes. Works properly.

--- app/app.js
+++ app/app.js
@@ -7,7 +7,10 @@ class App extends Component {
             <Router history={hashHistory}>
                 <Route path='/' component={Container} >
                     <IndexRoute component={Home} />
-                    <Route path='/address' component={Address} />
+                    <Route path='/address' component={Address}>
+                        <IndexRoute component={TwitterFeed} />
+                        <Route path='instagram' component={Instagram} />
+                    </Route>
                     <Route path='*' component={NotFound} />
                 </Route>
             </Router>
@@ -16,8 +19,18 @@ class App extends Component {
 }
 
 const Home = () => <h1>Hello from Home!</h1>
-const Address = () => <h1>We are in SF</h1>
+const Address = (props) => (
+    <div>
+        <br />
+        <Link to='/address'>Twitter Feed</Link>&nbsp;
+        <Link to='address/instagram'>Instagram Feed</Link>
+        <h1>We are in SF</h1>
+        {props.children}
+    </div>
+)
 const NotFound = () => (<h1>404... Page not found!</h1>)
+const Instagram = () => <h3>Instagram Feed</h3>
+const TwitterFeed = () => <h3>Twitter Feed</h3>
 const Nav = () => (
     <div>
         <Link to='/'>Home</Link>&nbsp;
