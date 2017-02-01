73e248030ad6f5e6a825bbedde99e96db98afa1a
Eric Eakin <eseakin@gmail.com>
2017-02-01 03:11:08 -0800

    Cleaned up code and fixed navbar bug with active class. Rearranged Query into Address submenu.

--- app/app.js
+++ app/app.js
@@ -8,7 +8,7 @@ class App extends Component {
                 <Route path='/' component={Container} >
                     <IndexRoute component={Home} />
                     <Route path='/address' component={Address}>
-                        <IndexRoute component={TwitterFeed} />
+                        <Route path='twitter' component={TwitterFeed} />
                         <Route path='instagram' component={Instagram} />
                         <Route path='query' component={Query} />
                     </Route>
@@ -23,43 +23,51 @@ class App extends Component {
     }
 }
 
-const Home = () => <h1>Hello from Home!</h1>
-const Address = (props) => (
-    <div>
-        <br />
-        <IndexLink className='nav' activeClassName='active' to='/address'>Twitter Feed</IndexLink>&nbsp;
-        <IndexLink className='nav' activeClassName='active' to='address/instagram'>Instagram Feed</IndexLink>
-        <h1>We are in SF</h1>
-        {props.children}
-    </div>
-)
-const NotFound = () => (<h1>404... Page not found!</h1>)
-const Instagram = () => <h3>Instagram Feed</h3>
-const TwitterFeed = () => <h3>Twitter Feed</h3>
-const Nav = () => (
-    <div>
-        <IndexLink className='nav' activeClassName='active' to='/'>Home</IndexLink>&nbsp;
-        <IndexLink className='nav' activeClassName='active' to='/address'>Address</IndexLink>
-        <IndexLink className='nav' activeClassName='active' to='/about'>About</IndexLink>
-        <IndexLink className='nav' activeClassName='active' to='/namedComponent'>Named Components</IndexLink>
-        <IndexLink className='nav' activeClassName='active' to={{
-            pathname: '/address/query',
-            query: {message: 'Hello from Route Query'}
-        }}>Route Query</IndexLink>
-    </div>
-)
 const Container = (props) => (
     <div>
         <Nav />
         {props.children}
     </div>
 )
+
+const Nav = () => (
+    <div>
+        <IndexLink className='nav' activeClassName='active' to='/'>Home</IndexLink>&nbsp;
+        <Link className='nav' activeClassName='active' to='/address'>Address</Link>
+        <IndexLink className='nav' activeClassName='active' to='/about'>About</IndexLink>
+        <IndexLink className='nav' activeClassName='active' to='/namedComponent'>Named Components</IndexLink>
+    </div>
+)
+
+const Home = () => <h1>Hello from Home!</h1>
+
+const Address = (props) => (
+    <div>
+        <br />
+        <IndexLink className='nav' activeClassName='active' to='/address/twitter'>Twitter Feed</IndexLink>&nbsp;
+        <IndexLink className='nav' activeClassName='active' to='address/instagram'>Instagram Feed</IndexLink>
+        <IndexLink className='nav' activeClassName='active' to={{
+            pathname: '/address/query',
+            query: {message: 'Hello from Route Query'}
+        }}>Route Query</IndexLink>
+        <h1>We are in SF</h1>
+        {props.children}
+    </div>
+)
+const Instagram = () => <h3>Instagram Feed</h3>
+const TwitterFeed = () => <h3>Twitter Feed</h3>
+const Query = (props) => (
+  <h2>{props.location.query.message}</h2>
+)
+
+
 const About = (props) => (
     <div>
         <h3>Welcome to the About Page</h3>
         {props.params.name && <h2>Hello, {props.params.name}</h2>}
     </div>
 )
+
 const NamedComponents = (props) => (
     <div>
         {props.title}<br />
@@ -70,11 +78,9 @@ const Title = () => (
     <h1>Hello from Title Component</h1>
 )
 const SubTitle = () => (
-    <h1>Hello from subTitle Component</h1>   
+    <h2>Hello from subTitle Component</h2>   
 )
 
-const Query = (props) => (
-  <h2>{props.location.query.message}</h2>
-)
+const NotFound = () => (<h1>404... Page not found!</h1>)
 
 export default App
