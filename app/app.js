441af54b330d2c3a915cad5ce57276017e306bd1
Eric Eakin <eseakin@gmail.com>
2017-02-01 01:56:42 -0800

    Added routes for a nav bar and 404. Nav bar shows on all pages.

--- app/app.js
+++ app/app.js
@@ -1,4 +1,34 @@
-import React, { Component } from 'react'
-import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
-const App = () => <h1>Hello World!</h1>
+import React, { Component } from 'react';
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router';
+
+class App extends Component {
+    render() {
+        return (
+            <Router history={hashHistory}>
+                <Route path='/' component={Container} >
+                    <IndexRoute component={Home} />
+                    <Route path='/address' component={Address} />
+                    <Route path='*' component={NotFound} />
+                </Route>
+            </Router>
+        );
+    }
+}
+
+const Home = () => <h1>Hello from Home!</h1>
+const Address = () => <h1>We are in SF</h1>
+const NotFound = () => (<h1>404... Page not found!</h1>)
+const Nav = () => (
+    <div>
+        <Link to='/'>Home</Link>&nbsp;
+        <Link to='/address'>Address</Link>
+    </div>
+)
+const Container = (props) => (
+    <div>
+        <Nav />
+        {props.children}
+    </div>
+)
+
 export default App
